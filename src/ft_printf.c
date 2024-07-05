6a39033a88b684aea4a7c4423594c8743ef8cae4
tsukasakop <tsukasakop@gmail.com>
2024-07-06 03:28:57 +0900

    ✨ Implement ft_printf

---
+++ src/ft_printf.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkondo <tkondo@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/05 22:41:55 by tkondo            #+#    #+#             */
+/*   Updated: 2024/07/06 03:13:42 by tkondo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "_stdio.ft.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+int	ft_printf(const char *format, ...)
+{
+	va_list	args;
+	int		n;
+
+	va_start(args, format);
+	n = ft_vfprintf(stdout, format, args);
+	va_end(args);
+	return n;
+}
