88ffad0391d1deca8abb5ea17cf1ce4f77427514
tsukasakop <tsukasakop@gmail.com>
2024-07-19 21:43:21 +0900

    🏷️ Change return type on ft_fwrite to ssize_t

--- include/_stdio.ft.h
+++ include/_stdio.ft.h
@@ -6,7 +6,7 @@
 /*   By: tkondo <tkondo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/06 03:11:49 by tkondo            #+#    #+#             */
-/*   Updated: 2024/07/06 03:11:55 by tkondo           ###   ########.fr       */
+/*   Updated: 2024/07/19 21:37:45 by tkondo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define _STDIO_FT_H
 # include <stdio.h>
 
-size_t	ft_fwrite(const void *ptr, size_t size, size_t n_items, FILE *stream);
+ssize_t	ft_fwrite(const void *ptr, size_t size, size_t n_items, FILE *stream);
 int		ft_printf(const char *format, ...);
 int		ft_vfprintf(FILE *s, const char *format, va_list ap);
 
--- src/ft_fwrite.c
+++ src/ft_fwrite.c
@@ -6,21 +6,14 @@
 /*   By: tkondo <tkondo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/06 01:02:25 by tkondo            #+#    #+#             */
-/*   Updated: 2024/07/06 03:13:25 by tkondo           ###   ########.fr       */
+/*   Updated: 2024/07/19 21:41:49 by tkondo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "_stdio.ft.h"
-#include <stdarg.h>
-#include <stdio.h>
 #include <unistd.h>
+#include <stdio.h>
 
-// works only stream is stdout
-size_t	ft_fwrite(const void *ptr, size_t size, size_t n_items, FILE *stream)
+ssize_t	ft_fwrite(const void *ptr, size_t size, size_t n_items, FILE *stream)
 {
-	size_t	towrite;
-
-	towrite = size * n_items;
-	write(stream->_file, ptr, towrite);
-	return (towrite);
+	return write(stream->_file, ptr, size*n_items);
 }
