3626adb32a3824750013e0c0a491af463ed46353
tsukasakop <tsukasakop@gmail.com>
2024-07-20 01:34:33 +0900

    ✨ Enable %% parse

--- src/ft_vfprintf.c
+++ src/ft_vfprintf.c
@@ -6,12 +6,29 @@
 /*   By: tkondo <tkondo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/05 22:41:55 by tkondo            #+#    #+#             */
-/*   Updated: 2024/07/19 21:51:39 by tkondo           ###   ########.fr       */
+/*   Updated: 2024/07/20 01:26:59 by tkondo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "_stdio.ft.h"
+#include "libft.h"
 #include <limits.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int	print_percent(char **f, FILE *s)
+{
+	++*f;
+	return (ft_fwrite("%", 1, 1, s));
+}
+
+int	print_fmt(FILE *s, char **f, va_list ap)
+{
+	if (**fmt == '%')
+		return (print_percent(f, s));
+	(void)ap;
+	return (0);
+}
 
 int	ft_vfprintf(FILE *s, const char *format, va_list ap)
 {
@@ -23,11 +40,16 @@ int	ft_vfprintf(FILE *s, const char *format, va_list ap)
 	tmp = (char *)format;
 	while (*tmp != '\0')
 	{
-		_cnt = ft_fwrite(tmp, 1, 1, s);
-		if(_cnt == -1 || cnt >= INT_MAX - _cnt)
-			return -1;
-		tmp++;
-		cnt+=_cnt;
+		if (*tmp != '%')
+			_cnt = ft_fwrite(tmp++, 1, 1, s);
+		else
+		{
+			++tmp;
+			_cnt = print_fmt(s, &tmp, ap);
+		}
+		if (_cnt == -1 || cnt >= INT_MAX - _cnt)
+			return (-1);
+		cnt += _cnt;
 	}
 	(void)ap;
 	return (cnt);
