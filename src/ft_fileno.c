fc729c3d2ec264d5255b028b7707f4441b152429
tsukasakop <tsukasakop@gmail.com>
2024-08-28 22:25:24 +0900

    ➕ Export feature for get fileno

--- src/ft_fileno.c
+++ src/ft_fileno.c
@@ -1,33 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_fwrite.c                                        :+:      :+:    :+:   */
+/*   ft_fileno.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tkondo <tkondo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/06 01:02:25 by tkondo            #+#    #+#             */
-/*   Updated: 2024/08/25 16:24:18 by tkondo           ###   ########.fr       */
+/*   Created: 2024/08/25 15:50:13 by tkondo            #+#    #+#             */
+/*   Updated: 2024/08/25 16:22:47 by tkondo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <unistd.h>
-#include "_stdio.ft.h"
+#include "_stdio.h"
 
-size_t	ft_fwrite(const void *ptr, size_t size, size_t n_items, FILE *stream)
+int	ft_fileno(FILE *stream)
 {
-	size_t	cnt;
-	ssize_t	writen;
-	int	fn;
-
-	fn = ft_fileno(stream);
-	cnt = 0;
-	while (cnt < n_items)
-	{
-		writen = write(fn, ptr + cnt, size);
-		if (writen < 0 || (size_t)writen < size)
-			break ;
-		cnt++;
-	}
-	return (cnt);
+	return (0);
 }
