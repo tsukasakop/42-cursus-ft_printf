397fcd2af1acbf5207c8665cf574965d6f29663a
tsukasakop <tsukasakop@gmail.com>
2024-09-04 23:22:52 +0900

    🏗️ Fix archtectual changes on test dir

--- test/src/test.c
+++ test/src/test.c
@@ -28,9 +28,9 @@ void	putstr_caret(char *s)
 			write(1, "\x1B[30;47m", 8);
 			write(1, c, 2);
 			write(1, "\x1B[0m", 4);
-			if(*s == '\0')
-				break;
 		}
+		if(*s == '\0')
+			break;
 		s++;
 	}
 }
@@ -43,13 +43,12 @@ void	test(char *str)
 	printf("Case: ");
 	fflush(stdout);
 	putstr_caret(s);
-	printf("\n");
-	printf("\x1B[31m");
+	printf("\x1B[31m\n");
 	std = printf(s);
-	printf("\x1B[32m");
+	printf("\x1B[32m\n");
 	ft = ft_printf(s);
-	printf("\x1B[0m");
+	printf("\x1B[0m\n");
 	char *result = std == ft ? "\x1B[32mSUCCESS\x1B[0m" : "\x1B[31mFAILUE\x1B[0m";
-	printf("\nresult: %s\n(ret) \x1B[31mstd\x1B[0m:%d, \x1B[32mft\x1B[0m:%d\n", result, std, ft);
+	printf("result: %s\n(ret) \x1B[31mstd\x1B[0m:%d, \x1B[32mft\x1B[0m:%d\n", result, std, ft);
 	free(s);
 }
